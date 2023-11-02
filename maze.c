3a8071a2536b52b8ad76a2670a0627714b44fd65
Petr Spac <petrik.spac@gmail.com>
2023-11-02 11:03:42 +0100

    feat: clean maze testing, start rpath

--- maze.c
+++ maze.c
@@ -18,11 +18,18 @@ typedef struct {
   unsigned char *cells;
 } Map;
 
+typedef struct {
+  int x;
+  int y;
+} coordinates_t;
+
 bool isborder(Map *map, int r, int c, int border);
 bool valid_borders(unsigned char cell, unsigned char *neighbors);
 void print_help();
 int start_border(Map *map, int r, int c, int leftright);
-bool maze_test(char *file_name);
+int maze_test(char *file_name);
+void rpath(Map *map, coordinates_t start);
+void lpath(Map *map, coordinates_t start);
 
 // TODO: remove later
 void showbits(unsigned char x) {
@@ -39,36 +46,45 @@ int main(int argc, char **argv) {
     print_help();
     return 0;
   } else if (argc == 3 && !strcmp(argv[1], "--test")) {
-    fprintf(stderr, "Running maze test\n");
+    pmesg("Running maze test %d\n", 0);
     maze_test(argv[2]);
     return 0;
+  } else if (argc == 5 && !strcmp(argv[1], "--rpath")) {
+    printf("RPATH");
+    // rpath()
+  } else {
+    fprintf(stderr, "Invalid argument\n");
   }
 
-  pmesg("Hello world %d\n", 0);
+  pmesg("Hello world argc: %d\n", argc);
   return 0;
 }
 
-/* Returns false for INVALID maze file
- */
-bool maze_test(char *file_name) {
-  FILE *fptr;
-
+int open_file(FILE *fptr, char *file_name) {
   fptr = fopen(file_name, "r");
   if (fptr == NULL) {
     fprintf(stderr, "Error opening file!\n");
-    return false;
+    return 1;
   }
+  return 0;
+}
 
-  int size[2] = {0};
+int load_maze_size(FILE *fptr, int rows, int cols) { return 0; }
+
+/* Returns 1 as failure, 0 for success
+ * TODO: Split into more functions
+ */
+int maze_test(char *file_name) {
+  // Load file
+  FILE *fptr;
+
+  open_file(fptr, file_name);
 
   // Load size
+  int size[2] = {0};
+
   for (int i = 0; i < 2; i++) {
     fscanf(fptr, "%d", &size[i]);
-  }
-
-  if (size[0] == 0 || size[1] == 0) {
-    fprintf(stderr, "Size is 0x0?\n");
-    return false;
   }
 
   // Test size & load maze
@@ -79,12 +95,13 @@ bool maze_test(char *file_name) {
   int tmp = 0;
   while (fscanf(fptr, "%d", &tmp) != EOF) {
     if (tmp < 0 || tmp > 255) {
-      fprintf(stderr, "Number for cell is out of range!\n");
+      fprintf(stderr, "Invalid\n");
+      return 1;
     }
 
     if (count >= (maze.rows * maze.cols)) {
-      fprintf(stderr, "Invalid size! %d\n", maze.rows * maze.cols);
-      return false;
+      fprintf(stderr, "Invalid\n");
+      return 1;
     }
 
     // Save value
@@ -96,15 +113,18 @@ bool maze_test(char *file_name) {
   pmesg("rows: %d, columns: %d\n", maze.rows, maze.cols);
   pmesg("count: %d\n", count);
 
-  //  get neigbourts around one cell
+  // get neigbourts around one cell
   for (int i = 0; i < maze.rows; i++) {
     for (int j = 0; j < maze.cols; j++) {
       unsigned char neighbors[3];
 
+      // Left
       neighbors[0] = (j > 0) ? maze.cells[(i * maze.cols) + j - 1] : 10;
+      // Right
       neighbors[1] =
           (j < maze.cols - 1) ? maze.cells[(i * maze.cols) + j + 1] : 10;
 
+      // Top/down
       if ((((i * maze.cols) + j) % 2) == 0) {
         neighbors[2] = (i > 0) ? maze.cells[(i - 1) * maze.cols + j] : 10;
       } else {
@@ -112,40 +132,31 @@ bool maze_test(char *file_name) {
             (i < maze.rows - 1) ? maze.cells[(i + 1) * maze.cols + j] : 10;
       }
 
-      valid_borders(maze.cells[i * maze.cols + j], neighbors);
-
-      // pmesg("%d ",
-      //       maze.cells[i * maze.cols +
-      //                  j]); // current row * max column + current column
+      if (!valid_borders(maze.cells[i * maze.cols + j], neighbors)) {
+        fprintf(stderr, "Invalid\n");
+        return 1;
+      }
     }
-    printf("\n");
   }
-
   fclose(fptr);
-  return true;
+
+  fprintf(stdout, "Valid\n");
+  return 0;
 }
 
 bool valid_borders(unsigned char cell, unsigned char *neighbors) {
-  for (int i = 0; i < 3; i++) {
-    printf("neighbor: %d\n", neighbors[i]);
-  }
-
   // Left neighbor
   if (neighbors[0] != 10 && (((cell >> 0) & 1) != ((neighbors[0] >> 1) & 1))) {
-    fprintf(stderr, "Invalid left border\n");
     return false;
   }
   // Right neighbor
   if (neighbors[1] != 10 && (((cell >> 1) & 1) != ((neighbors[1] >> 0) & 1))) {
-    fprintf(stderr, "Invalid right border\n");
     return false;
   }
   // Up/Down neighbor
   if (neighbors[2] != 10 && (((cell >> 2) & 1) != ((neighbors[2] >> 2) & 1))) {
-    fprintf(stderr, "Invalid up/down border\n");
     return false;
   }
-  printf("\n");
 
   return true;
 }
@@ -162,7 +173,7 @@ bool isborder(Map *map, int r, int c, int border) {
 void print_help() {
   char *help = "maze [command] <options>\n\
 \n\
---test <file> check, if given file has needed maze definition. If the maze file is valid program pritns VALID. Otherwise prints INVALID\n\
+--test <file> check, if given file has needed maze definition. If the maze file is valid program pritns Valid. Otherwise prints Invalid\n\
 --rpath <R> <C> tries to find path in maze on entry on column R and row C. Path is searched by rule of right hand\n\
 --lpath <R> <C> trues to find path in maze on entry on column R and row C. Path is searched by the rule of left hand";
 
